875d1ae50baab5e0973f5a4346c91550f3e5009d
AlisonZ <alisonkzerbe@gmail.com>
2017-06-14 11:36:28 -0700

    set up views and app.js with imports

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@
 // Import jQuery & Underscore
 import $ from 'jquery';
 import _ from 'underscore';
-import Movie from ''
+import Movie from './models/movie.js';
+import MovieList from './collections/movie_list.js';
+import MovieView from './views/movie_view.js';
+import MovieListView from './view/movie_list_view.js';
 
 // ready to go
 $(document).ready(function() {
