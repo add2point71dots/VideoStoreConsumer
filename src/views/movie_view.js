9228f1ae188f8853eab17dcd903e57e7370541c9
AlisonZ <alisonkzerbe@gmail.com>
2017-06-19 16:08:07 -0700

    updates movie view

--- src/views/movie_view.js
+++ src/views/movie_view.js
@@ -7,7 +7,6 @@ var MovieView = Backbone.View.extend({
     initialize: function(params){
         this.template = params.template;
         this.searched = params.searched;
-        this.detailsClicked = params.detailsClicked;
         this.listenTo(this.model, 'change', this.render);
     },
 
