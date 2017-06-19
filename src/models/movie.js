f9c58d37f96799d026a4aa98cc2cc811256b779b
AlisonZ <alisonkzerbe@gmail.com>
2017-06-19 14:10:16 -0700

    making calls to API using title

--- src/models/movie.js
+++ src/models/movie.js
@@ -1,10 +1,13 @@
 import Backbone from 'backbone';
 
 var Movie = Backbone.Model.extend({
-  sync: function(method, model, options) {
-    options.url = model.collection.url + '/' + model.attributes.title;
-    return Backbone.sync(method, model, options);
-  }
+    sync: function(method, model, options) {
+        console.log('method::  ', method);
+        if (method=='read'){
+            options.url = model.collection.url + '/' + model.attributes.title;
+        }
+        return Backbone.sync(method, model, options);
+    }
 });
 
 export default Movie;
